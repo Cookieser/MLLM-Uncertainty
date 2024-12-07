3375929da0a4127c316f50d34fb184a08ee80084
Yupu Wang <324424398@qq.com>
2024-12-07 11:42:45 -0500

    mllm_plus

--- semantic_uncertainty/generate_answers_mllm.py
+++ semantic_uncertainty/generate_answers_mllm.py
@@ -16,13 +16,41 @@ from uncertainty.data.data_utils import load_ds
 from uncertainty.utils import utils
 from uncertainty.uncertainty_measures import p_true as p_true_utils
 from compute_uncertainty_measures import main as main_compute
-from dataset_handle import dataset_handler
-from few_shot_handle import few_shot_handler
-from p_true_handle import p_true_handler
-from generation_handle import generation_handler
+
 utils.setup_logger()
 
+def dataset_handler(args):
+    # Setup run.
+    if args.dataset == 'svamp':
+        if not args.use_context:
+            logging.info('Forcing `use_context=True` for svamp dataset.')
+            args.use_context = True
+    elif args.dataset == 'squad':
+        if not args.answerable_only:
+            logging.info('Forcing `answerable_only=True` for squad dataset.')
+            args.answerable_only = True
 
+    # Load dataset.
+    train_dataset, validation_dataset = load_ds(
+        args.dataset, add_options=args.use_mc_options, seed=args.random_seed)
+    if args.ood_train_dataset is not None:
+        logging.warning(
+            'Using OOD dataset %s to construct few-shot prompts and train p_ik.',
+            args.ood_train_dataset)
+        # Get indices of answerable and unanswerable questions and construct prompt.
+        train_dataset, _ = load_ds(args.ood_train_dataset, add_options=args.use_mc_options)
+    if not isinstance(train_dataset, list):
+        logging.info('Train dataset: %s', train_dataset)
+
+    # Get indices of answerable and unanswerable questions and construct prompt.
+    answerable_indices, unanswerable_indices = utils.split_dataset(train_dataset)
+
+    if args.answerable_only:
+        unanswerable_indices = []
+        val_answerable, val_unanswerable = utils.split_dataset(validation_dataset)
+        del val_unanswerable
+        validation_dataset = [validation_dataset[i] for i in val_answerable]
+    return train_dataset,validation_dataset,answerable_indices,unanswerable_indices
 
 
 def few_shot(args, train_dataset, prompt_indices):
